ac8205108f93edb480562423c706854bad65dd5c
excel-azmfin <excel.azmin@gmail.com>
2022-07-28 16:52:01 +0600

    create element from js

--- index.js
+++ index.js
@@ -1,52 +1,9 @@
+// const h1 = document.createElement("h1")
+// h1.textContent = " Hello React H1 Tag"
+// h1.className = "header"
 
-function Navbar(){
-    return (
-       <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav> 
-    )
-    
-}
-
-function HaderText(){
-    return (<h1>Hello Hader</h1>)
-}
+// document.getElementById("root").append(h1)
 
 ReactDOM.render(
-    <div>
-        <Navbar />,
-        <HaderText />
-    </div>, 
-    document.getElementById("root")
+    <h1 className="hader"> Hello React!</h1>, document.getElementById("root")
 )
